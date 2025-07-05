b242fa5cb21d2973495b998c753c23329722d916
Taresh Rajput <58368119+taresh18@users.noreply.github.com>
2025-07-05 08:28:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 export HF_HOME="/workspace/hf"
 export HF_TOKEN="<your-token-here>"
 #export TRANSFORMERS_OFFLINE=1
-uvicorn main:app --host localhost --port 9090
+uvicorn main:app --host 0.0.0.0 --port 9090
